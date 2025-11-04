4cf78a3058c53cf85b2685f4af7b7b396478041c
Chandushree-BM <chandushreebm2002@gmail.com>
2025-11-04 15:09:28 +0530

    signup page modifications

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
+import Landing from './pages/Landing'
 
 function App() {
   return (
+    <>
     <BrowserRouter>
     <Routes>
       <Route path='/signup' element={<Signup/>}/>
       <Route path = '/' element={<Signin/>}/> 
     </Routes>
     </BrowserRouter>
+    {/* <Landing/> */}
+    </>
   )
 }
 
